7a2b1a1910c942c734b72c17952de0bbb06ae3b0
kyuubi-Yhp <south01village@gmail.com>
2024-12-14 15:20:58 +0300

    add 4 sections

---
+++ script.js
@@ -0,0 +1,10 @@
+const a = document.querySelector('.menu__list')
+a.addEventListener('mousemove', popitmenu)
+
+
+
+function popitmenu() {
+
+    document.querySelector('.menu__list').style.width = '191px'
+  
+}
